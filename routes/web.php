8b11105be8c46399f200389f9258880fd8b4556c
Enkay <enkayx@icloud.com>
2025-07-19 18:57:06 +0200

    login page

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,20 @@
 <?php
 
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Change this route to use the ProductController index method
-Route::get('/', [ProductController::class, 'index'])->name('product.index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/product', [ProductController::class, 'index'])->name('product.index');
-Route::get('/product/add', [ProductController::class, 'add'])->name('product.add');
-Route::post('/product', [ProductController::class, 'store'])->name('product.store');
-Route::get('/product/edit/{id}', [ProductController::class, 'edit'])->name('product.edit');
-Route::patch('/product/update/{id}', [ProductController::class, 'update'])->name('product.update');
-Route::delete('/product/delete/{id}', [ProductController::class, 'destroy'])->name('product.delete');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
